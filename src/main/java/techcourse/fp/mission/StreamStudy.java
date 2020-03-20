6222a545071115b6f467a55cfd39ec6b58970217
soring <jnsorn@gmail.com>
2020-03-20 13:56:29 +0900

    feat : 스트림 API를 이용하여 파일 내의 문자를 조건에 맞게 추출

--- src/main/java/techcourse/fp/mission/StreamStudy.java
+++ src/main/java/techcourse/fp/mission/StreamStudy.java
@@ -6,51 +6,63 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class StreamStudy {
-    public static long countWords() throws IOException {
-        String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
-        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+	public static long countWords() throws IOException {
+		String contents = new String(Files.readAllBytes(Paths
+			.get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+		List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
-    }
+		long count = 0;
+		for (String w : words) {
+			if (w.length() > 12)
+				count++;
+		}
+		return count;
+	}
 
-    public static List<Integer> doubleNumbers(List<Integer> numbers) {
-        List<Integer> result = new ArrayList<>();
-        for(Integer number: numbers) {
-            result.add(2 * number);
-        }
+	public static List<Integer> doubleNumbers(List<Integer> numbers) {
+		List<Integer> result = new ArrayList<>();
+		for (Integer number : numbers) {
+			result.add(2 * number);
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-    public static long sumAll(List<Integer> numbers) {
-        int result = 0;
+	public static long sumAll(List<Integer> numbers) {
+		int result = 0;
 
-        for(Integer number: numbers) {
-            result += number;
-        }
+		for (Integer number : numbers) {
+			result += number;
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-    public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return numbers.stream().filter(v -> v > 3).mapToInt(v -> v * 2).sum();
-    }
+	public static long sumOverThreeAndDouble(List<Integer> numbers) {
+		return numbers.stream()
+			.filter(v -> v > 3)
+			.mapToInt(v -> v * 2)
+			.sum();
+	}
 
-    public static void printLongestWordTop100() throws IOException {
-        String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
-        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+	public static void printLongestWordTop100() throws IOException {
+		String contents = new String(Files.readAllBytes(Paths
+			.get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+		List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        System.out.println(words);
-        System.out.println(words.size());
-        // TODO 이 부분에 구현한다.
-    }
+		System.out.println(words);
+		System.out.println(words.size());
+
+		words.stream()
+			.filter(word -> word.length() > 12)
+			.sorted(Comparator.comparingInt(String::length).reversed())
+			.distinct()
+			.limit(100)
+			.map(String::toLowerCase)
+			.forEach(System.out::println);
+	}
 }
