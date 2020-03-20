642c1158795934b2a03d0a71e2de1b631752f287
soring <jnsorn@gmail.com>
2020-03-20 13:54:05 +0900

    feat : 람다를 활용하여 Calculator의 중복 제거

--- src/main/java/techcourse/fp/mission/Calculator.java
+++ src/main/java/techcourse/fp/mission/Calculator.java
@@ -3,29 +3,33 @@ package techcourse.fp.mission;
 import java.util.List;
 
 public class Calculator {
-    public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
-    }
+	public static int sum(List<Integer> numbers, Conditional conditional) {
+		return numbers.stream()
+			.filter(number -> conditional.test(number))
+			.mapToInt(Integer::intValue)
+			.sum();
+	}
 
-    public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
-    }
+	public static int sumAll(List<Integer> numbers) {
+		int total = 0;
+		for (int number : numbers) {
+			total += number;
+		}
+		return total;
+	}
 
-    public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
+	public static int sumAllEven(List<Integer> numbers) {
+		int total = 0;
+		for (int number : numbers) {
+			if (number % 2 == 0) {
+				total += number;
+			}
+		}
+		return total;
+	}
 
-        //TODO: List에 담긴 값 중 3보다 큰 수만을 더해야 한다.
-
-        return total;
-    }
+	public static int sumAllOverThree(List<Integer> numbers) {
+		int total = 0;
+		return total;
+	}
 }
--- src/test/java/techcourse/fp/mission/CalculatorTest.java
+++ src/test/java/techcourse/fp/mission/CalculatorTest.java
@@ -1,30 +1,30 @@
 package techcourse.fp.mission;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
-    private List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+	private List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
 
-    @Test
-    public void sumAll() {
-        int sum = Calculator.sumAll(numbers);
-        assertThat(sum).isEqualTo(21);
-    }
+	@Test
+	public void sumAll() {
+		int sum = Calculator.sum(numbers, number -> true);
+		assertThat(sum).isEqualTo(21);
+	}
 
-    @Test
-    public void sumAllEven() {
-        int sum = Calculator.sumAllEven(numbers);
-        assertThat(sum).isEqualTo(12);
-    }
+	@Test
+	public void sumAllEven() {
+		int sum = Calculator.sum(numbers, number -> number % 2 == 0);
+		assertThat(sum).isEqualTo(12);
+	}
 
-    @Test
-    public void sumAllOverThree() {
-        int sum = Calculator.sumAllOverThree(numbers);
-        assertThat(sum).isEqualTo(15);
-    }
+	@Test
+	public void sumAllOverThree() {
+		int sum = Calculator.sum(numbers, number -> number > 3);
+		assertThat(sum).isEqualTo(15);
+	}
 }
